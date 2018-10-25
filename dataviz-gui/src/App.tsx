59c5169805b37c140545629263881d1d254116a6
Andreas Ågren <andreas.agren@mindified.com>
2018-10-25 11:22:27 +0200

    Added axes

--- dataviz-gui/src/App.tsx
+++ dataviz-gui/src/App.tsx
@@ -15,37 +15,48 @@ const convertRow =  (d: d3.DSVRowString):IParsedRow  =>  {
 };
 
 const renderChart = (
-  barHeight: number, 
-  scaler: d3.ScaleLinear<number, number>, 
-  chart: d3.Selection<d3.BaseType, {}, HTMLElement, any>) =>
-    (data: d3.DSVParsedArray<IParsedRow>) => {
-      scaler.domain([0, d3.max(data.map(d => d.value)) || 0]);
-      chart.attr("height", barHeight * data.length);
-      const bar = chart.selectAll("g")
-          .data(data)
-        .enter().append("g")
-          .attr("transform", (d, i) => `translate(0, ${i * barHeight})`);
-      bar.append("rect")
-        .attr("width", d => scaler(d.value))
-        .attr("height", barHeight - 1)
+  height: number,
+  y: d3.ScaleLinear<number, number>,
+  x: d3.ScaleBand<string>,
+  chart: d3.Selection<d3.BaseType, {}, HTMLElement, any>,
+  xAxis: d3.Axis<string>,
+  yAxis: d3.Axis<number | {valueOf(): number}>) =>
+  (data: d3.DSVParsedArray<IParsedRow>) => {
+    x.domain(data.map(d => d.name));
+    y.domain([0, d3.max(data, d => d.value) || 0]);
+    chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+    chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+    chart.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.name) || 0)
+        .attr("y", d => y(d.value))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.value))
         .style("fill", "steelblue");
-      bar.append("text")
-        .attr("x", d => scaler(d.value) - 3)
-        .attr("y", barHeight / 2)
-        .attr("dy", ".33em")
-        .style("text-anchor", "end")
-        .style("fill", "white")
-        .text(d => d.value);
-    };
+  };
 
 class App extends React.Component {
   public componentDidMount() {
-    const width = 420;
-    const barHeight = 20;
+    const margin = {top: 20, right: 30, bottom: 30, left: 40};
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+    const y = d3.scaleLinear().range([height, 0]);
+    const x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
     const chart = d3.select("#chart")
-        .attr("width", width);
-    const scaler = d3.scaleLinear().range([0, width]);
-    d3.csv("data.csv", convertRow).then(renderChart(barHeight, scaler, chart));
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    d3.csv("data.csv", convertRow).then(renderChart(height, y, x, chart, xAxis, yAxis));
   }
   public render() {
     return (
