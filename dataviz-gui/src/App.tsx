6afc34a357c79da1051bdd0bdcf5d4f271ffa182
Andreas Ågren <andreas.agren@mindified.com>
2018-10-29 14:59:28 +0100

    Start center in nyc

--- dataviz-gui/src/App.tsx
+++ dataviz-gui/src/App.tsx
@@ -7,7 +7,7 @@ import * as secrets from './secrets.json';
 
 class App extends React.Component {
   public componentDidMount() {
-    const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    const mymap = L.map('mapid').setView([40.758161, -73.981927], 12);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       accessToken: secrets.mapbox,
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
