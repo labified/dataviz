d545215725969abcd584622be33c0648c913dddf
Andreas Ågren <andreas.agren@mindified.com>
2018-10-31 10:07:05 +0100

    Overlay with first 1000 pickups

--- dataviz-gui/src/App.tsx
+++ dataviz-gui/src/App.tsx
@@ -1,3 +1,4 @@
+import * as d3 from 'd3';
 import * as L from 'leaflet';
 import * as React from 'react';
 
@@ -5,15 +6,105 @@ import './App.css';
 import logo from './mindified-logo.png';
 import * as secrets from './secrets.json';
 
-class App extends React.Component {
-  public componentDidMount() {
-    const mymap = L.map('mapid').setView([40.758161, -73.981927], 12);
+interface IParsedRow extends d3.DSVRowAny {
+  index: number,
+  id: string,
+  vendor_id: number,
+  pickup_datetime: Date,
+  dropoff_datetime: Date,
+  passenger_count: number,
+  pickup_longitude: number,
+  pickup_latitude: number,
+  dropoff_longitude: number,
+  dropoff_latitude: number,
+  store_and_fwd_flag: boolean,
+  trip_duration: number
+}
+
+const parseRows = (row: d3.DSVRowString) : IParsedRow => {
+  return {
+    index: +(row.index || 0),
+    id: row.id || "",
+    vendor_id: +(row.vendor_id || 0),
+    pickup_datetime: new Date(row.pickup_datetime || 0),
+    dropoff_datetime: new Date(row.dropoff_datetime || 0),
+    passenger_count: +(row.passenger_count || 0),
+    pickup_longitude: +(row.pickup_longitude || 0),
+    pickup_latitude: +(row.pickup_latitude || 0),
+    dropoff_longitude: +(row.dropoff_longitude || 0),
+    dropoff_latitude: +(row.dropoff_latitude || 0),
+    store_and_fwd_flag: (row.store_and_fwd_flag || 'N') === 'Y',
+    trip_duration: +(row.trip_duration || 0)
+  };
+};
+
+function geoTransform(map: L.Map) {
+  return d3.geoTransform({
+    point: function projectPoint(x: number, y: number) {
+      const point = map.latLngToLayerPoint(new L.LatLng(y, x));
+      this.stream.point(point.x, point.y);
+    }
+  });
+}
+
+const toGeoJsonFeatures = (row: IParsedRow): GeoJSON.Feature => {
+  return {
+    type: "Feature",
+    geometry: {
+        coordinates: [row.pickup_longitude, row.pickup_latitude],
+        type: "Point"
+    },
+    id: row.index,
+    properties: null
+  } as GeoJSON.Feature;
+};
+
+const renderMap = () => {
+  const map = L.map('mapid').setView([40.758161, -73.981927], 12);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       accessToken: secrets.mapbox,
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       id: 'mapbox.streets',
       maxZoom: 18
-    }).addTo(mymap);
+    }).addTo(map);
+  const svg = d3.select(map.getPanes().overlayPane).append("svg");
+  const g = svg.append("g").attr("class", "leaflet-zoom-hide");
+  d3.csv("train_1000.csv", parseRows)
+    .then(trips => {
+      const transform = geoTransform(map);
+      const path = d3.geoPath().projection(transform);
+      
+      const geoData = { type: "FeatureCollection", features: trips.map(toGeoJsonFeatures) } as d3.ExtendedFeatureCollection;
+
+      const feature = g.selectAll("path")
+          .data(geoData.features)
+        .enter().append("path");
+
+        map.on("moveend", reset);
+        reset();
+
+        // Reposition the SVG to cover the features.
+        function reset() {
+          global.console.log("reset called");
+          const bounds = path.bounds(geoData);
+          const topLeft = bounds[0];
+          const bottomRight = bounds[1];
+
+          svg .attr("width", bottomRight[0] - topLeft[0])
+              .attr("height", bottomRight[1] - topLeft[1])
+              .style("left", `${topLeft[0]}px`)
+              .style("top", `${topLeft[1]}px`);
+
+          g   .attr("transform", `translate(${-topLeft[0]},${-topLeft[1]})`);
+
+          feature.attr("d", path);
+        }
+     });
+};
+
+class App extends React.Component {
+  public componentDidMount() {
+    renderMap();
   }
   public render() {
     return (
